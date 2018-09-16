c85fe7bf21a40a7209a8b3e6d061d7a15da7db95
ds-suhrid <suhrid.sarkar@datasoft-bd.com>
2018-09-16 21:39:00 +0600

    added jdbc demo project

--- jdbcdemo/src/test/java/com/datasoft/spring/training/jdbcdemo/JdbcdemoApplicationTests.java
+++ jdbcdemo/src/test/java/com/datasoft/spring/training/jdbcdemo/JdbcdemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.datasoft.spring_demo;
+package com.datasoft.spring.training.jdbcdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringDemoApplicationTests {
+public class JdbcdemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
